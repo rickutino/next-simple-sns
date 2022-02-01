7faa79b4ac3d0d97b2d982ef0c517adb8a1de39f
rickutino <rickutino@gmail.com>
2022-02-01 23:13:11 +0900

    Loginに移動

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,3 @@
-import StyledButton from "../components/Button";
-import { Button } from '@material-ui/core';
-
 export default function Home() {
-  return (
-    <>
-      <Button variant="contained" color="secondary">Material-UI</Button>
-      <StyledButton>Hello World</StyledButton>
-    </>
-  );
+  return <div>Home</div>
 }
