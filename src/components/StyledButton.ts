aa73c65eeac6cb560bce591e58c5ca93777fb096
rickutino <rickutino@gmail.com>
2022-02-01 23:02:18 +0900

    component/input作成

---
+++ src/components/StyledButton.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+import Button from '@material-ui/core/Button';
+
+const StyledButton = styled(Button)`
+  background: ${props => props.theme.palette.secondary.main};
+  border-radius: 5px;
+  border: 1px black ;
+  color: ${props => props.theme.palette.grey[200]};
+  height: 48px;
+  weight: 325px;
+  margin-top: 1rem;
+  padding: 0 30px;
+  &&:hover {
+    background: ${props => props.theme.palette.secondary.light};
+  }
+`;
+
+export default StyledButton;
