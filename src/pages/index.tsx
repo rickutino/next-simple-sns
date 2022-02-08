4bb66a4de0691c30384bdf161a2f924e53a72532
rickutino <rickutino@gmail.com>
2022-02-08 23:29:12 +0900

    ログインした後のPosts一覧表とCurrentUserの情報ページ(スタイル未実装)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,43 @@
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+interface User {
+  name: string;
+  email: string;
+  iconImageUrl?: string | null
+}
+
+interface Posts {
+  id: number;
+  body: string;
+}
 
 export default function Home() {
-  const { user } = useContext(AuthContext);
+  const [user, setUser] = useState<User>()
+  const [posts, setPosts] = useState<Posts[]>([])
+
+
+  useEffect(() => {
+    api.get('/posts').then(response => {
+
+      setPosts(response.data.posts);
+    });
+
+    api.get('/account').then(response => {
+      const { name, email, iconImageUrl } = response.data.user;
+
+      setUser({ name, email, iconImageUrl })
+    })
+  },[]);
 
   return (
     <>
-      <img src={user?.iconImageUrl} alt="User Image"/>
-      <h1>Post: {user?.email}</h1>
+      <img src={user.iconImageUrl} alt="User Image" />
+      <h1>name: {user.name}</h1>
+
+      {posts.map((post) => (
+        <div key={post.id}>post: {post.body}</div>
+      ))}
     </>
   );
 }
