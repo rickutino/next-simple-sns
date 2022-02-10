14cdfbf0264bbbba33501dd5765eccbb60c9c759
rickutino <rickutino@gmail.com>
2022-02-11 00:44:22 +0900

    refactoring user icon path (途中未完成)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import Router from "next/router";
+import { AuthContext } from '../contexts/AuthContext';
 import { api } from '../services/api';
+import Link from 'next/link';
 
 interface User {
   name: string;
@@ -13,27 +16,43 @@ interface Posts {
 }
 
 export default function Home() {
+  // const { singOut } = useContext(AuthContext);
   const [user, setUser] = useState<User>()
   const [posts, setPosts] = useState<Posts[]>([])
+
 
 
   useEffect(() => {
     api.get('/posts').then(response => {
 
       setPosts(response.data.posts);
+    }).catch((error) => {
+      console.log(error)
     });
 
     api.get('/account').then(response => {
       const { name, email, iconImageUrl } = response.data.user;
-
+      console.log(name)
       setUser({ name, email, iconImageUrl })
-    })
+    }).catch((error) => {
+      console.log(error);
+    });
   },[]);
 
   return (
     <>
-      <img src={user.iconImageUrl} alt="User Image" />
-      <h1>name: {user.name}</h1>
+      {posts.length <= 0 &&
+        <h2>
+          投稿がありません初めての投稿者になりませんか？
+          <Link href="/post">
+            <a>Let's Post</a>
+          </Link>
+        </h2>
+      }
+      {user?.iconImageUrl  == null &&
+        <img src="../profileIcon.png" alt="User Icon" />
+      }
+      <h1>name: {user?.name}</h1>
 
       {posts.map((post) => (
         <div key={post.id}>post: {post.body}</div>
