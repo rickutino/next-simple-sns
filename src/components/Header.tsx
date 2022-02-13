4d5c621313604a6aa3f0994211ad1f79642ac82e
rickutino <rickutino@gmail.com>
2022-02-13 18:48:30 +0900

    add Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,82 @@
+import Link from 'next/link';
+import { useContext, useEffect, useState } from 'react';
+import { FiHome, FiPower } from 'react-icons/fi';
+import { RiAccountCircleLine } from 'react-icons/ri';
+import { AuthContext } from '../contexts/AuthContext';
+import Notification from './Notification';
+import { api } from '../services/api';
+import { Container, HeaderContent, Profile, Info } from '../styles/Header';
+import ConfirmDialog from './ConfirmDialog';
+
+interface User {
+  name: string;
+  email: string;
+  iconImageUrl?: string | null
+}
+
+export default function Header() {
+  const { signOut, notify, setNotify, confirmDialog, setConfirmDialog } = useContext(AuthContext);
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    api.get('/account').then(response => {
+      const { name, email, iconImageUrl } = response.data.user;
+
+      setUser({ name, email, iconImageUrl })
+    }).catch((error) => {
+      console.log(error);
+    });
+  },[]);
+
+  return (
+    <>
+      <Container>
+        <HeaderContent>
+          <img
+            src={'/logo.svg'}
+            alt="simple-sns"
+          />
+          <Profile>
+            <img src={
+              user?.iconImageUrl
+              ? user.iconImageUrl
+              : `/icons/profileIcon.png` }
+            />
+            <Info>
+              <span>Welcome, </span>
+              <Link href="/profile">
+                <strong>{user?.name}</strong>
+              </Link>
+            </Info>
+          </Profile>
+
+          <nav>
+            <Link href="/">
+              <FiHome />
+            </Link>
+            <Link href="/">
+              <RiAccountCircleLine />
+            </Link>
+            <button onClick={() => {
+              setConfirmDialog({
+                isOpen: true,
+                title: "ログアウトしてもよろしいでしょうか？",
+                onConfirm: () => {signOut()}
+              });
+            }} >
+              <FiPower color={"#E0483D"}/>
+            </button>
+          </nav>
+        </HeaderContent>
+      </Container>
+      <Notification
+        notify={notify}
+        setNotify={setNotify}
+      />
+      <ConfirmDialog
+        confirmDialog={confirmDialog}
+        setConfirmDialog={setConfirmDialog}
+      />
+    </>
+  )
+}
---
+++ src/styles/Header.ts
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  padding: 32px 0;
+  background:  ${props => props.theme.palette.primary.main};
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  > img {
+    height: 42px;
+  }
+  nav {
+    margin-left: auto;
+    display: flex;
+    height: 100%;
+    width: 265px;
+    align-items: center;
+    justify-content: space-between;
+    svg {
+      color: ${props => props.theme.palette.grey[200]};
+      width: 24px;
+      height: 24px;
+      cursor: pointer;
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+    button {
+      background: transparent;
+      border: 0;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 48px;
+  img {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+  }
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 16px;
+  line-height: 24px;
+  span {
+    color: #999591;
+  }
+  strong {
+    color: ${props => props.theme.palette.secondary.main};
+  }
+  a {
+    text-decoration: none;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
