23b7a4b58135fa16323b8bf4f499abe33e7065e4
rickutino <rickutino@gmail.com>
2022-03-23 00:27:12 +0900

    ProfileページのRefactoringとEslintの自動修正

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,112 +1,89 @@
 import {
   Avatar,
-  Card,
-  CardContent,
-  CardHeader,
-  CardMedia,
+  Box,
   Container,
   Link,
-  Typography,
-  Theme
-} from "@mui/material";
+  styled,
+  Typography
+} from '@mui/material';
 import { GetServerSideProps } from 'next';
-
-import { makeStyles } from "@mui/styles";
-import { Box } from "@mui/system";
-import { useContext, useEffect, useState } from "react";
-import { Header, BottomHeaderNavigation } from "../../components/Header";
-import Notification from "../../components/Notification";
-import { AuthContext } from "../../contexts/AuthContext";
 import { parseCookies } from 'nookies';
-import { api } from "../../services/api";
+import { useContext } from 'react';
+import { BottomHeaderNavigation, Header } from '../../components/Header';
+import Notification from '../../components/Notification';
+import { AuthContext } from '../../contexts/AuthContext';
+import theme from '../../styles/theme';
 
-interface User {
-  id: string;
-  name: string;
-  email: string;
-  iconImageUrl: string | null;
-}
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    background: theme.palette.primary.main,
-    boxShadow: 'none',
-  },
-  text: {
-    color: theme.palette.secondary.main,
-  },
-  subText: {
-    color: theme.palette.grey[400],
-  },
-  avatar: {
-    margin: '0 auto',
-  }
-}));
+const Root = styled(Box)({
+  background: theme.palette.primary.main,
+  boxShadow: 'none',
+  height: '100vh'
+});
 
 export default function Profile() {
-  const { notify, setNotify } = useContext(AuthContext);
-  const [currentUser, setCurrentUser] = useState<User>();
-  const classes = useStyles();
-
-
-  useEffect(() => {
-    api.get("/account").then(response => {
-      console.log(response.data.user);
-      setCurrentUser(response.data.user);
-    })
-  }, []);
+  const { notify, setNotify, user } = useContext(AuthContext);
 
   return (
-    <div className={classes.root}>
+    <Root>
       <Header />
 
-      <Container maxWidth="md" className={classes.root}>
+      <Container maxWidth="md">
         <Link href="/profile/update">
-          <Typography variant="h4" align="right" className={classes.text} mb={2} mt={4}>
+          <Typography
+            variant="h4"
+            align="right"
+            sx={{ color: theme.palette.secondary.main }}
+            mb={4}
+            mt={6}
+          >
             編集
           </Typography>
         </Link>
         <Avatar
-          className={classes.avatar}
-          alt={currentUser?.name}
-          sx={{ width: 185, height: 185 }}
-          src={
-            currentUser?.iconImageUrl
-            ? currentUser.iconImageUrl
-            : `/icons/profileIcon.png` }
+          sx={{ margin: '0 auto', width: 185, height: 185 }}
+          alt={user?.name}
+          src={user?.iconImageUrl}
         />
         <Box>
-          <Typography variant="h4" align="center" className={classes.text} mt={8} mb={2}>
-            {currentUser?.name}
+          <Typography
+            variant="h4"
+            align="center"
+            sx={{ color: theme.palette.secondary.main }}
+            mt={8}
+            mb={2}
+          >
+            {user?.name}
           </Typography>
-          <Typography variant="h6" align="center" className={classes.subText} mb={2}>
-            {currentUser?.email}
+          <Typography
+            variant="h6"
+            align="center"
+            sx={{ color: theme.palette.grey[400] }}
+            mb={2}
+          >
+            {user?.email}
           </Typography>
         </Box>
       </Container>
 
       <BottomHeaderNavigation />
-      <Notification
-        notify={notify}
-        setNotify={setNotify}
-      />
-    </div>
-  )
+      <Notification notify={notify} setNotify={setNotify} />
+    </Root>
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { ['next-simple-sns']: token } = parseCookies(ctx)
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const { 'next-simple-sns': token } = parseCookies(ctx);
 
   if (!token) {
     return {
       redirect: {
         destination: '/account/login',
-        permanent: false,
+        permanent: false
       }
-    }
+    };
   }
 
   return {
     props: {}
-  }
-}
+  };
+};
