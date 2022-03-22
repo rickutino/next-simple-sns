164ec3f96ad12dbb04b31a90fe9988c0139981ee
Ricardo Utino <rickutino@gmail.com>
2022-03-22 08:16:30 +0000

    Eslintの自動修正PostComponent

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,16 +1,23 @@
-import { Box, Button, Container, TextField, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  styled,
+} from '@mui/material';
 
 import { GetServerSideProps } from 'next';
-import { useRouter } from "next/router";
-import { FormEvent, useContext, useState } from "react";
+import { useRouter } from 'next/router';
+import { FormEvent, useContext, useState } from 'react';
 
-import { Header, BottomHeaderNavigation } from "../../components/Header";
-
-import Notification from "../../components/Notification";
-import { AuthContext } from "../../contexts/AuthContext";
 import { parseCookies } from 'nookies';
-import { api } from "../../services/api";
-import theme from "../../styles/theme";
+import { Header, BottomHeaderNavigation } from '../../components/Header';
+
+import Notification from '../../components/Notification';
+import { AuthContext } from '../../contexts/AuthContext';
+import { api } from '../../services/api';
+import theme from '../../styles/theme';
 
 const PostRoot = styled(Box)({
   backgroundColor: theme.palette.primary.main,
@@ -28,11 +35,11 @@ const Form = styled('form')({
     paddingTop: '4rem',
   },
 });
- 
+
 const Input = styled(TextField)({
   borderRadius: '5px',
   backgroundColor: theme.palette.grey[200],
-})
+});
 
 const PostButton = styled(Button)({
   borderRadius: '5px',
@@ -43,75 +50,77 @@ const PostButton = styled(Button)({
 
 export default function Post() {
   const { notify, setNotify } = useContext(AuthContext);
-  const [ post, setPost ] = useState('');
-  const [ countLength, setCountLength ] = useState<Number>(0);
-  const [ sendButtonDisabled, setSendButtonDisabled ] = useState(true);
-  const [ inputError, setInputError ] = useState(false);
+  const [post, setPost] = useState('');
+  const [countLength, setCountLength] = useState<number>(0);
+  const [sendButtonDisabled, setSendButtonDisabled] = useState(true);
+  const [inputError, setInputError] = useState(false);
 
   const router = useRouter();
 
-  function handleChange ( event: React.ChangeEvent<HTMLInputElement> ) {
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setCountLength(event.target.value.length);
     setPost(event.target.value);
     setSendButtonDisabled(false);
   }
 
-  async function handleSubmit ( event: FormEvent) {
+  async function handleSubmit(event: FormEvent) {
     event.preventDefault();
     setInputError(false);
 
-    if(post == '') {
+    if (post === '') {
       setInputError(true);
       setNotify({
         isOpen: true,
-        message: "コメントは必須です。",
-        type: 'error'
+        message: 'コメントは必須です。',
+        type: 'error',
       });
 
       return;
     }
 
     setSendButtonDisabled(true);
-    try{
+    try {
       await api.post('/posts', {
         post: {
           body: post,
-        }
+        },
       });
 
       setNotify({
         isOpen: true,
-        message: "投稿を成功しました。",
-        type: 'success'
+        message: '投稿を成功しました。',
+        type: 'success',
       });
 
       router.push('/');
-    }catch (error) {
+    } catch (error) {
       setNotify({
         isOpen: true,
         message: `${error}`,
-        type: 'error'
+        type: 'error',
       });
     }
   }
- 
+
   return (
     <PostRoot>
       <Header />
-      <Container maxWidth='md'>
+      <Container maxWidth="md">
         <Form onSubmit={e => handleSubmit(e)}>
-          <Typography variant="body1" align="right" >{countLength}</Typography>
+          <Typography variant="body1" align="right">
+            {countLength}
+          </Typography>
           <Input
             placeholder="メッセージの入力待ち..."
             variant="outlined"
             multiline
             fullWidth
             rows={5}
-            onChange={handleChange}
+            onChange={() => handleChange}
             inputProps={{ maxLength: 140 }}
-            error={inputError || post.length == 140 ? true : false}
+            error={!!(inputError || post.length === 140)}
           />
-         <Box mt={3}>
+          <Box mt={3}>
             <PostButton
               type="submit"
               variant="contained"
@@ -125,27 +134,24 @@ export default function Post() {
       </Container>
 
       <BottomHeaderNavigation />
-      <Notification
-        notify={notify}
-        setNotify={setNotify}
-      />
+      <Notification notify={notify} setNotify={setNotify} />
     </PostRoot>
-  )
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { ['next-simple-sns']: token } = parseCookies(ctx)
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const { 'next-simple-sns': token } = parseCookies(ctx);
 
   if (!token) {
     return {
       redirect: {
         destination: '/account/login',
         permanent: false,
-      }
-    }
+      },
+    };
   }
 
   return {
-    props: {}
-  }
-}
+    props: {},
+  };
+};
