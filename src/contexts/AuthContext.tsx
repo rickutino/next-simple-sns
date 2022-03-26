3c437bbe4bf64f1d97c1e13be530d9aadcc808d9
rickutino <rickutino@gmail.com>
2022-03-26 22:43:08 +0900

    ログインユーザーのIDをStateに渡していなかった為の変更

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,14 +1,20 @@
-import Router, { useRouter } from "next/router";
-import { destroyCookie, setCookie } from "nookies";
-import { createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
-import { api } from "../services/api";
-
+import Router, { useRouter } from 'next/router';
+import { destroyCookie, setCookie } from 'nookies';
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState
+} from 'react';
+import { api } from '../services/api';
 
 interface User {
-  id?: string;
-  name?: string;
+  id: string;
+  name: string;
   email: string;
-  iconImageUrl?: string | null;
+  iconImageUrl: string | null;
 }
 interface Notification {
   isOpen: boolean;
@@ -17,10 +23,10 @@ interface Notification {
 }
 
 interface DialogData {
-  isOpen: boolean,
+  isOpen: boolean;
   title: string;
   onConfirm?(): void;
-};
+}
 
 interface LoginCredentials {
   email: string;
@@ -36,7 +42,7 @@ interface SingUpCredentials {
 interface AuthContextData {
   login(credentials: LoginCredentials): Promise<void>;
   singUp(credentials: SingUpCredentials): Promise<void>;
-  signOut(): void,
+  signOut(): void;
   user: User;
   setUser: Dispatch<SetStateAction<User>>;
   notify: Notification;
@@ -49,89 +55,108 @@ interface AuthProviderProps {
   children: ReactNode;
 }
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [notify, setNotify] = useState({isOpen: false, message: "", type: null})
-  const [confirmDialog, setConfirmDialog] = useState({ isOpen: false, title: ""});
+  const [notify, setNotify] = useState({
+    isOpen: false,
+    message: '',
+    type: null
+  });
+  const [confirmDialog, setConfirmDialog] = useState({
+    isOpen: false,
+    title: ''
+  });
   const [user, setUser] = useState<User>();
   const router = useRouter();
 
   useEffect(() => {
-    window.addEventListener('popstate', function(e) {
+    window.addEventListener('popstate', () => {
       router.push('/');
     });
 
     return () => {
-      window.removeEventListener('popstate', function(e) {});
+      window.removeEventListener('popstate', () => {});
     };
   }, []);
 
   useEffect(() => {
-    api.get('/account').then(response => {
-      const { name, email, iconImageUrl } = response.data.user;
+    api
+      .get('/account')
+      .then(response => {
+        const { id, name, email, iconImageUrl } = response.data.user;
 
-      setUser({ name, email, iconImageUrl })
-    }).catch((error) => {
-      console.log(error);
-    });
+        setUser({ id, name, email, iconImageUrl });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }, []);
 
   function signOut() {
     setConfirmDialog({
       ...confirmDialog,
       isOpen: false
-    })
-
-    api.delete('/auth').then(() => {
-      destroyCookie(undefined, 'next-simple-sns');
-
-      Router.push('/account/login');
-
-      setNotify({
-        isOpen: true,
-        message: "ログアウトしました、また遊びに来てください。",
-        type: 'success'
-      });
-    }).catch(() => {
-      setNotify({
-        isOpen: true,
-        message: "お手数ですが再ログインしていただくかしばらくお待ちください。",
-        type: 'error'
-      });
-
-      return;
     });
+
+    api
+      .delete('/auth')
+      .then(() => {
+        destroyCookie(undefined, 'next-simple-sns');
+
+        Router.push('/account/login');
+
+        setNotify({
+          isOpen: true,
+          message: 'ログアウトしました、また遊びに来てください。',
+          type: 'success'
+        });
+      })
+      .catch(() => {
+        setNotify({
+          isOpen: true,
+          message:
+            'お手数ですが再ログインしていただくかしばらくお待ちください。',
+          type: 'error'
+        });
+      });
   }
 
   async function login({ email, password }: LoginCredentials) {
-    await api.post('auth', {
-      email,
-      password
-    }).then(response => {
-      const { token, user: { name, iconImageUrl}} = response.data;
+    await api
+      .post('auth', {
+        email,
+        password
+      })
+      .then(response => {
+        const {
+          token,
+          user: { name, iconImageUrl }
+        } = response.data;
 
-      setCookie(undefined, 'next-simple-sns', token, {
-        maxAge: 60 * 60 * 24, //24 hours;
-        path: '/' //どのパスがこのクッキーをアクセスできるか。
+        setCookie(undefined, 'next-simple-sns', token, {
+          maxAge: 60 * 60 * 24, // 24 hours;
+          path: '/' // どのパスがこのクッキーをアクセスできるか。
+        });
+
+        setUser({
+          name,
+          email,
+          iconImageUrl
+        });
+
+        // Headerにあるtokenの更新
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
+        router.push('/');
+      })
+      .catch(() => {
+        setNotify({
+          isOpen: true,
+          message: 'メールアドレスかパスワードが間違っています',
+          type: 'error'
+        });
       });
-
-      setUser({
-        name, email, iconImageUrl
-      });
-
-      // Headerにあるtokenの更新
-      api.defaults.headers['Authorization'] = `Bearer ${token}`
-
-      router.push('/')
-
-    }).catch(() => {
-      setNotify({
-        isOpen: true,
-        message: "メールアドレスかパスワードが間違っています",
-        type: 'error'
-      });
-    });
   }
 
   async function singUp({ name, email, password }: SingUpCredentials) {
@@ -150,36 +175,38 @@ export function AuthProvider({ children }: AuthProviderProps) {
       const { token } = response.data;
 
       setCookie(undefined, 'next-simple-sns', token, {
-        maxAge: 60 * 60 * 24, //24 hours;
-        path: '/' //どのパスがこのクッキーをアクセスできるか。
+        maxAge: 60 * 60 * 24, // 24 hours;
+        path: '/' // どのパスがこのクッキーをアクセスできるか。
       });
 
       // Headerにあるtokenアクセスの更新をさせる。
-      api.defaults.headers['Authorization'] = `Bearer ${token}`
+      api.defaults.headers.Authorization = `Bearer ${token}`;
 
-      router.push('/')
+      router.push('/');
     } catch (error) {
       setNotify({
         isOpen: true,
-        message: "このメールアドレスは既に使われています。",
+        message: 'このメールアドレスは既に使われています。',
         type: 'error'
       });
     }
   }
 
   return (
-    <AuthContext.Provider value={{
-      login,
-      singUp,
-      signOut,
-      user,
-      setUser,
-      notify,
-      setNotify,
-      confirmDialog,
-      setConfirmDialog
-    }}>
-      { children }
+    <AuthContext.Provider
+      value={{
+        login,
+        singUp,
+        signOut,
+        user,
+        setUser,
+        notify,
+        setNotify,
+        confirmDialog,
+        setConfirmDialog
+      }}
+    >
+      {children}
     </AuthContext.Provider>
-  )
+  );
 }
