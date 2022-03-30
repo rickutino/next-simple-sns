536328dd50763031b03354b69ade8770de0d41ba
rickutino <rickutino@gmail.com>
2022-03-30 20:17:33 +0900

    parseCookiesからTokenをうまく引っ張り出せない問題。:

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,11 +4,12 @@ import { destroyCookie, parseCookies } from 'nookies';
 
 export function getAPIClient(ctx?: any) {
   const cookies = parseCookies(ctx);
+  const { 'next-simple-sns': token } = parseCookies(ctx);
 
   const api = axios.create({
     baseURL: 'https://simp-340605.an.r.appspot.com/',
     headers: {
-      Authorization: `Bearer ${cookies['next-simple-sns']}`
+      Authorization: `Bearer ${token}`
     }
   });
 
@@ -18,6 +19,10 @@ export function getAPIClient(ctx?: any) {
     },
     (error: AxiosError) => {
       if (error.response.status === 401) {
+        console.log('erro', error.response);
+        console.log('cookies', cookies);
+        console.log('token', token);
+        console.log('parseCookies', parseCookies(ctx));
         if (error.response.statusText === 'Unauthorized') {
           destroyCookie(undefined, 'next-simple-sns');
 
