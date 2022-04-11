b23bc55e6b2eb91f17ea2be5c18ddee52da24dd9
rickutino <rickutino@gmail.com>
2022-04-12 03:02:30 +0900

    Interceptor.requestをAuthContextからAxiosのファイルに変更

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,15 +1,22 @@
+/* eslint-disable dot-notation */
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import Router from 'next/router';
 import { destroyCookie, parseCookies } from 'nookies';
+import { tokenKey } from '../shared/const';
 
 export function getAPIClient(ctx?: any) {
-  const { 'next-simple-sns': token } = parseCookies(ctx);
-
   const api = axios.create({
-    baseURL: 'https://simp-340605.an.r.appspot.com/',
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
+    baseURL: 'https://simp-340605.an.r.appspot.com/'
+  });
+
+  api.interceptors.request.use(request => {
+    const userToken = parseCookies(ctx);
+
+    request.headers['Authorization'] = userToken[tokenKey]
+      ? `Bearer ${userToken[tokenKey]}`
+      : '';
+
+    return request;
   });
 
   api.interceptors.response.use<AxiosResponse>(
